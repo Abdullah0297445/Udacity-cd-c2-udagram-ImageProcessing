c62ae06aaeb5677741e7604728310208cb07a273
Abdullah Ajmal <abdullah.ajmal.7@hotmail.com>
2021-07-31 14:07:12 +0500

    Added core filter method

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filter } from 'bluebird';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file
+  app.get("/filteredimage",
+    async (req, res) => {
+      const url = req.query.image_url
+      if (!url) {
+        res.status(400).send('image_url missing.')
+      }
+      console.log(url);
+      const path = await filterImageFromURL(url)
+      if (path !== "err") {
+        res.sendFile(path, () => {
+          deleteLocalFiles([path]);
+        });
+      } else {
+        res.status(400).send("Image not found.")
+      }
+    });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
